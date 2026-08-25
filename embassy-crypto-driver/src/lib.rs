c1b1298212e08080b30a3391465b3d1e5671e0c2
xoviat <xoviat@users.noreply.github.com>
2026-08-25 17:27:37 -0500

    remove hashcontext

--- embassy-crypto-driver/src/lib.rs
+++ embassy-crypto-driver/src/lib.rs
@@ -79,20 +79,6 @@ impl Algorithm<'_> {
 }
 
 // ------------------------------------------------------------------
-// HashContext
-// ------------------------------------------------------------------
-
-/// Opaque context buffer for streaming hash operations.
-#[derive(Clone, Copy)]
-pub struct HashContext(pub [u8; 256]);
-
-impl Default for HashContext {
-    fn default() -> Self {
-        Self([0u8; 256])
-    }
-}
-
-// ------------------------------------------------------------------
 // ContextHandle
 // ------------------------------------------------------------------
 
